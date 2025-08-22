ee6ba92f15b3d162a349e413ef99c98df458f880
daianebadilhocortez <daianebadilho@gmail.com>
2025-08-21 21:57:11 -0300

    inclusão exercício 6

--- aula2-ex5.js
+++ aula2-ex5.js
@@ -2,7 +2,7 @@
 // separe os dígitos em variáveis diferentes 
 // (centena, dezena, unidade) u lizando apenas divisões e restos aritmé cos. 
 
-let numero = Number(prompt("Digite um número que corresponda a uma centena"));
+let numero = Number(prompt("Digite um número inteiro de três digitos"));
 console.log("Você digitou o número " + numero);
 
 
@@ -11,10 +11,13 @@ const centena = (numero - (numero % 100)) / 100;
 const dezena = ((numero % 100) - (numero % 10)) / 10;
 const unidade = numero % 10;
 
-
-
 console.log("O seu número corresponde a centena de " + centena + " a dezena de " + dezena + " e a únidade de " + unidade);
 
 
+// exercício 6: reconstrua o número.
+
+let reconstrucao = (centena * 100) + (dezena * 10) + unidade;
+
+console.log("O seu número reconstruído é " + reconstrucao);
 
 
